83645034fbe45e91327ffb39897e5eea6296a17d
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2022-03-18 17:14:39 +0100

    Adding treatment of various SEXP types

--- src/ast.cpp
+++ src/ast.cpp
@@ -52,21 +52,69 @@ std::string const write_escaped(std::string const& s) {
   return out;
 }
 
+inline void write_edge(std::ostream& stream,  uint64_t from_id, uint64_t to_id) {
+  stream << " v_" << from_id << " -> v_" << to_id << ";" << std::endl;
+}
 
-void traverse_ast(std::ostream& stream, const std::string& parent_name, SEXP ast) {
+
+void traverse_ast(std::ostream& stream, const std::string& role, uint64_t& id, SEXP ast) {
+  uint64_t my_id = id;
+  //start writing the node
+  stream << " v_" << my_id << " [label=\"";
+
   switch(TYPEOF(ast)) {
   case EXPRSXP: {
+      stream << "expression\"];" << std::endl;
 
+      size_t nb_exprs = Rf_xlength(ast);
+      for(size_t i = 0 ; i < nb_exprs ; i++) {
+        id++;
+        write_edge(stream, my_id, id);
+        traverse_ast(stream, "", id, VECTOR_ELT(ast, i));
+      }
     }
     break;
 
   case LANGSXP: {
+      // TODO: Special treatment for control instructions (if, while, repeat and so on)
+      if(role != "") {
+        stream << role << ": ";
+      }
+      stream  << "call\"];" << std::endl;
+      // call name
+      id++;
+      write_edge(stream, my_id, id);
+      traverse_ast(stream, "call name", id, CAR(ast));
+
+      // arguments
+      size_t i = 0;
+      for(SEXP cons = CDR(ast) ; cons != R_NilValue; cons = CDR(cons), i++) {
+        id++;
+        std::string arg = std::string("arg_") + std::to_string(i);
+        write_edge(stream, my_id, id);
+        traverse_ast(stream, arg, id, CAR(cons) );
+      }
     }
     break;
   case SYMSXP: {
-
+      // symbols are red
+      stream << CHAR(PRINTNAME(ast)) << "\", color = red];" << std::endl;
     }
     break;
+  case NILSXP: {
+      //litterals are blue
+      stream << "NULL\", color = blue];" << std::endl;
+    }
+    break;
+  case INTSXP: {
+    stream << Rf_asInteger(ast) << " : int\", color = blue];" << std::endl;
+    }
+    break;
+  case REALSXP: {
+    stream << Rf_asReal(ast) << " : real\", color = blue];" << std::endl;
+    }
+    break;
+    // TODO: other sexps
   }
 }
 
